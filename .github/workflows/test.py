da66badb2add55e5c2365a1cdc77c378da00c711
baltas99 <82153483+baltas99@users.noreply.github.com>
2023-11-11 17:20:21 +0200

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import requests
 import weather
-from Currency import convert_currency
 
 weather.get_weather('Limassol')
 # weather.get_weather_for_city()
 
-convert_currency()
