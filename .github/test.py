458713e3ebb1572747ae971a770ba778a6e8a4cf
baltas99 <82153483+baltas99@users.noreply.github.com>
2023-11-11 17:20:55 +0200

    Add files via upload

---
+++ .github/test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import requests
+import weather
+from Currency import convert_currency
+
+weather.get_weather('Limassol')
+# weather.get_weather_for_city()
+
+convert_currency()
