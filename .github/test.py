48a7f27bebf10141f6b5f9a300d7702c14e994a5
baltas99 <82153483+baltas99@users.noreply.github.com>
2023-11-11 17:23:33 +0200

    Delete .github/test.py

--- .github/test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import requests
-import weather
-from Currency import convert_currency
-
-weather.get_weather('Limassol')
-# weather.get_weather_for_city()
-
-convert_currency()
