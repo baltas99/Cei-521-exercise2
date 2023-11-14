f292893d6132a2d18f5751ecf2fb09edaba45a45
AgiAnd <150835988+AgiAnd@users.noreply.github.com>
2023-11-14 19:39:59 +0200

    Update Currency.py

--- Currency.py
+++ Currency.py
@@ -1,55 +1,27 @@
-# currency.py
 import streamlit as st
 import requests
 
-# Function to check or initialize the button click state
 def check_button_state(key):
     if key not in st.session_state:
         st.session_state[key] = False
 
-def convert_currency():
-    api_key = "83754dc3d2-6c852bf831-s3rryq"
-
-    amount = st.number_input("Enter the amount to convert:", min_value=0.01, step=0.01, format="%.2f")
-
-    source_currency = st.selectbox("Select the source currency:", ["EUR", "USD", "JPY", "GBP"])
-
-    target_currency = st.selectbox("Select the target currency:", ["USD", "JPY", "EUR", "GBP"])
-
-    if st.button("Convert"):
-        if source_currency == target_currency:
-            st.warning("Source and target currencies are the same. No conversion needed.")
-        else:
-            base_url = "https://api.fastforex.io/convert"
-            params = {
-                "api_key": api_key,
-                "from": source_currency,
-                "to": target_currency,
-                "amount": amount,
-            }
-
-            response = requests.get(base_url, params=params)
-
-            if response.status_code == 200:
-                data = response.json()
-                converted_amount = data["result"]
-                st.success(f"{amount} {source_currency} is approximately {converted_amount} {target_currency}")
-            else:
-                st.error("Error while fetching data.")
-
 def convert_EUR():
+    check_button_state("eur_button_clicked")  # Ensure the session state key is initialized
+
     api_key = "83754dc3d2-6c852bf831-s3rryq"
     amount = st.number_input("Enter the amount to convert:", min_value=0.01, step=0.01, format="%.2f")
     target_currency = st.selectbox("Select the target currency:", ["USD", "JPY", "GBP"])
 
-    if st.button("Convert"):
+    if st.button("Convert EUR"):
+        st.session_state["eur_button_clicked"] = True  # Update session state on button click
+
         base_url = "https://api.fastforex.io/convert"
         params = {
             "api_key": api_key,
             "from": "EUR",
             "to": target_currency,
             "amount": amount,
-            }
+        }
 
         response = requests.get(base_url, params=params)
 
@@ -60,15 +32,8 @@ def convert_EUR():
         else:
             st.error("Error while fetching data.")
 
-def Currency():
-    st.title("Currency Converter")
-
-    option = st.selectbox("Select Conversion Method:", ["Convert Any Currency", "Convert from EUR"])
-
-    if option == "Convert Any Currency":
-        convert_currency()
-    elif option == "Convert from EUR":
-        convert_EUR()
+def main():
+    convert_EUR()  # Calling the function inside main
 
 if __name__ == "__main__":
-    Currency()
+    main()
