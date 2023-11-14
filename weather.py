89b3d8bc13faf74064843676b3c3b0a6a816a53b
AgiAnd <150835988+AgiAnd@users.noreply.github.com>
2023-11-14 19:38:49 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -26,24 +26,3 @@ def get_weather(city):
             st.write(f"Humidity: {weather_data['main']['humidity']}%")
         else:
             st.warning(f"Failed to retrieve weather data for {city}.")
-
-def get_weather_for_city():
-    check_button_state()
-    city = st.text_input("Enter a town name:")
-    if st.button("Search"):
-        st.session_state['button_clicked'] = True
-        # Your existing API call logic
-        api_key = "31061e71138b8a05356fba63f13034f8"
-        url = "https://api.openweathermap.org/data/2.5/weather"
-        params = {"q": city, "appid": api_key, "units": "metric"}
-
-        response = requests.get(url, params=params)
-
-        if response.status_code == 200:
-            weather_data = response.json()
-            st.write(f"Town: {city}")
-            st.write(f"Temperature: {weather_data['main']['temp']}°C")
-            st.write(f"Description: {weather_data['weather'][0]['description']}")
-            st.write(f"Humidity: {weather_data['main']['humidity']}%")
-        else:
-            st.warning(f"Failed to retrieve weather data for {city}.")
