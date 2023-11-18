daa281d7a552aab3123bedd8bf96182e6e477982
AgiAnd <150835988+AgiAnd@users.noreply.github.com>
2023-11-18 15:39:50 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,27 +2,19 @@ import streamlit as st
 import requests
 import os
 
-# Function to check or initialize the button click state
-def check_button_state():
-    if 'button_clicked' not in st.session_state:
-        st.session_state['button_clicked'] = False
-
 def get_weather(city):
-    check_button_state()
-    if st.button(f"Get Weather for {city}"):
-        st.session_state['button_clicked'] = True
-        # Your existing API call logic
-        api_key = "31061e71138b8a05356fba63f13034f8"
-        url = "https://api.openweathermap.org/data/2.5/weather"
-        params = {"q": city, "appid": api_key, "units": "metric"}
+    st.session_state.button_clicked = True  # Simulate the button click
 
-        response = requests.get(url, params=params)
+    api_key = "31061e71138b8a05356fba63f13034f8"
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": api_key, "units": "metric"}
 
-        if response.status_code == 200:
-            weather_data = response.json()
-            st.write(f"Town: {city}")
-            st.write(f"Temperature: {weather_data['main']['temp']}°C")
-            st.write(f"Description: {weather_data['weather'][0]['description']}")
-            st.write(f"Humidity: {weather_data['main']['humidity']}%")
-        else:
-            st.warning(f"Failed to retrieve weather data for {city}.")
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        st.write(f"Temperature: {weather_data['main']['temp']}°C")
+        st.write(f"Description: {weather_data['weather'][0]['description']}")
+        st.write(f"Humidity: {weather_data['main']['humidity']}%")
+    else:
+        st.warning(f"Failed to retrieve weather data for {city}.")
